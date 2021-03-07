a70fe3d64d9c14f836aae2b377bb64eca819c2d0
Ismail <ismailparsola@gmail.com>
2021-03-07 23:06:15 +0530

    handled requests

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   },[])
   useEffect(() => {
     const fetchApi = async () => {
-      const res = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${field}&appid=9d3b063799ebb1b67bf96d024de5eddc`)
+      const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${field}&appid=9d3b063799ebb1b67bf96d024de5eddc`)
       const resJson = await res.json()
       
       await setcity(resJson.main || null)
